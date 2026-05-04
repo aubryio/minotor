f55dca2280bc0b5f4b937a00be4d3e78b0cece39
Aubry Cholleton <aubry@cholleton.com>
2026-05-04 21:36:31 +0200

    fix typing.

--- src/__e2e__/router.test.ts
+++ src/__e2e__/router.test.ts
@@ -3,7 +3,10 @@ import { describe, it } from 'node:test';
 
 import fs from 'fs';
 
-import type { ServiceRouteInfo, TransferTypeString } from '../router.js';
+import type {
+  ServiceRouteInfo,
+  TransferType as TransferTypeString,
+} from '../router.js';
 import {
   Query,
   RangeQuery,
